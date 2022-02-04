8392ffb6a1e2cecec4c538b626ba81538184ba5a
bayamasa <mapi0213mgt@gmail.com>
2022-02-05 06:49:40 +0900

    MOD: exportのテスト用に修正

--- src/breakout.ts
+++ src/breakout.ts
@@ -91,24 +91,24 @@ class Ball {
 	}
 }
 
-class Paddle {
-	constructor(
-		private readonly height: number,
-		private readonly width: number,
-		private readonly x: number,
-		public rightPressed: boolean,
-		public leftPressed: boolean
-	) {}
-	public getHeight(): number {
-		return this.height;
-	}
-	public getWidth(): number {
-		return this.width;
-	}
-	public getX(): number {
-		return this.x;
-	}
-}
+// class Paddle {
+// 	constructor(
+// 		private readonly height: number,
+// 		private readonly width: number,
+// 		private readonly x: number,
+// 		public rightPressed: boolean,
+// 		public leftPressed: boolean
+// 	) {}
+// 	public getHeight(): number {
+// 		return this.height;
+// 	}
+// 	public getWidth(): number {
+// 		return this.width;
+// 	}
+// 	public getX(): number {
+// 		return this.x;
+// 	}
+// }
 
 class Brick {
 	constructor(
--- src/object.ts
+++ src/object.ts
@@ -1,22 +1,4 @@
-class Breakout {
-	constructor(
-		private readonly canvas: HTMLCanvasElement,
-		private readonly ctx: CanvasRenderingContext2D, 
-		private readonly score: number,
-		private readonly lives: number
-	) {}
-}
-
-export class Ball {
-	constructor(
-		private readonly x: number, 
-		private readonly y: number,
-		public dx: number, 
-		private dy :number
-	){}
-}
-
-class Paddle {
+export class Paddle {
 	constructor(
 		private readonly height: number,
 		private readonly width: number,
@@ -24,16 +6,13 @@ class Paddle {
 		public rightPressed: boolean,
 		public leftPressed: boolean
 	) {}
-}
-
-class Brick {
-	constructor(
-		private readonly rowCount: number,
-		private readonly columnCount: number,
-		private readonly width: number,
-		private readonly height: number,
-		private readonly padding: number,
-		private readonly offsetTop: number,
-		private readonly offsetLeft: number,
-	) {}
+	public getHeight(): number {
+		return this.height;
+	}
+	public getWidth(): number {
+		return this.width;
+	}
+	public getX(): number {
+		return this.x;
+	}
 }
